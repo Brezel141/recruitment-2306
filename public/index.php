e32f40b6d391a7591aa59bae1a82ca253e8bf663
Brezel141 <broggio.manuel@gmail.com>
2024-05-16 16:19:49 +0200

    moved the HTML to home View

--- public/index.php
+++ public/index.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <title>Home</title>
-</head>
-<body>
-    <h1>Willkommen auf der Startseite</h1>
-    <p>Klicken Sie auf die folgenden Links, um zu den Seiten zu gelangen:</p>
-    <ul>
-        <li><a href="/jobangebote">Gehe zu Jobangebote</a></li>
-        <li><a href="/bewerbung">Gehe zu Bewerbung</a></li>
-    </ul>
-</body>
-</html>
-
-
-
 <?php
 
 // Hier ist der  einstieg punkt unsere App
